6a5312d643e00bdc85ff380337b59b80e45b21b6
Izaias Machado <izaiasmachado.dev@gmail.com>
2022-12-02 23:39:17 -0300

    feat: docker mysql

--- src/server/src/index.js
+++ src/server/src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const app = require("./app");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const dbc = require("./services/databaseConnection");
 
 async function main() {
