ed7545df84db64f08705fe3f37a817312bbbe3c9
Izaias Machado <izaiasmachado.dev@gmail.com>
2022-12-04 20:28:19 -0300

    fix: remover quantidade dependentes formulário

--- src/client/script.js
+++ src/client/script.js
@@ -253,9 +253,6 @@ const postFuncionario = async () => {
     "salario_funcionario"
   ).value;
   const cargo_funcionario = document.getElementById("cargo_funcionario").value;
-  const quantidade_dependentes = document.getElementById(
-    "quantidade_dependentes"
-  ).value;
 
   const newPost = {
     matricula_funcionario: matricula_funcionario,
@@ -268,7 +265,6 @@ const postFuncionario = async () => {
     data_nascimento_funcionario: data_nascimento_funcionario,
     salario_funcionario: salario_funcionario,
     cargo_funcionario: cargo_funcionario,
-    quantidade_dependentes: quantidade_dependentes,
   };
 
   console.log(newPost);
